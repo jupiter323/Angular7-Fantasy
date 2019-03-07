33632c637de95947c47a8bc089fb5fe702418bdd
jupiter <jupiter.fierce.31@gmail.com>
2019-03-07 16:16:31 +0100

    js import module

--- src/app/fantasy/register/register.component.ts
+++ src/app/fantasy/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/global.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private gs:GlobalService
+  ) { }
 
   ngOnInit() {
+    this.loadScripts();
   }
-
+  private loadScripts() {
+    // You can load multiple scripts by just providing the key as argument into load method of the service
+    this.gs.load('chartjs', 'fantasy-register-pulse').then(data => {
+      // Script Loaded Successfully
+      console.log(data);
+    }).catch(error => console.log(error));
+  }
 }
